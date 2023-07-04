9201a005d5a2e473c15a30ee652967623e9f4b39
LA ROSA DANELA <danielaalarosa@gmail.com>
2023-07-04 12:22:25 -0300

    Proyecto Notas

---
+++ src/Data/notes.js
@@ -0,0 +1,17 @@
+export const notes = [
+  {
+    id: 0,
+    title: "Mi primera Nota",
+    description: "Este ejemplo es muy útil",
+  },
+  {
+    id: 1,
+    title: "Mi segunda Nota",
+    description: "Este ejemplo es útil",
+  },
+  {
+    id: 2,
+    title: "Mi tercera Nota",
+    description: "Este ejemplo es poco útil",
+  },
+];
